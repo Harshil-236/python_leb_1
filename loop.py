544f9fc778d1eb3cde57f7f8bdd6f677ab949310
Harshil-236 <bca2022harshil1780@tnraocollege.org>
2024-07-14 23:22:47 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -5,3 +5,7 @@ for i in range(1, 11):
 # Printing numbers from 10 to 1
 for i in range(10, 0, -1):
     print(i)
+
+output:- 
+
+![Screenshot (34)](https://github.com/user-attachments/assets/46efdabc-c76d-4064-9f92-f67235066708)
